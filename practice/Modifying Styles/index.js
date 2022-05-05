aa1f9053723e88d5f1c1d2bab03cb021418dc0c1
Alosha Calderon <aloshacalderon@gmail.com>
2022-05-05 10:03:35 -0500

    Modifying Styles: className & classList, completed

---
+++ practice/Modifying Styles/index.js
@@ -0,0 +1,12 @@
+var header = document.getElementById("header")
+header.style.backgroundColor = "aqua"
+
+
+document.body.style.backgroundColor = "pink"
+
+
+// fontSize
+// backgroundColor
+// display
+// textAlign
+// margin, padding, border
---
+++ practice/Modifying Styles: className & classList/index.js
@@ -0,0 +1,9 @@
+// className
+// document.querySelector("#header").className += " new-class"
+
+//classList 
+console.log(document.querySelector("#header").className)
+document.querySelector("#header").classList.toggle("another")
+console.log(document.querySelector("#header").className)
+document.querySelector("#header").classList.toggle("another")
+console.log(document.querySelector("#header").className)
