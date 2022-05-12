085b01299ce91ef99db6fe6650fd617e465fbd51
Alosha Calderon <aloshacalderon@gmail.com>
2022-05-12 08:51:57 -0500

    DJ JS, done

---
+++ practice/DS JS - Event listener practice/index.js
@@ -0,0 +1,33 @@
+//Mouse Over and Out Function
+document.getElementById("demo").addEventListener("mouseover", mouseOver);
+    function mouseOver() {
+        document.getElementById("demo").style.backgroundColor = "Blue";
+    }
+
+document.getElementById("demo").addEventListener("mouseout", mouseOut);
+  function mouseOut() {
+    document.getElementById("demo").style.backgroundColor = "White";
+    }
+
+// Mouse Held Down Function
+document.getElementById("demo").addEventListener("mousedown", mouseDown);
+  function mouseDown() {
+      document.getElementById("demo").style.backgroundColor = "Red";
+    }
+
+// Mouse Button is Let Go
+document.getElementById("demo").addEventListener("mouseup", mouseUp);
+    function mouseUp() {
+        document.getElementById("demo").style.backgroundColor = "Yellow";
+    }
+
+// Mouse is Double Clicked
+document.getElementById("demo").addEventListener("dblclick", ondblclick);
+    function ondblclick() {
+        document.getElementById("demo").style.backgroundColor = "Green";
+    }
+
+// Mouse Scroll is used somewhere in the window
+    window.onwheel = function() {
+        document.getElementById("demo").style.backgroundColor = "Orange";
+    }
