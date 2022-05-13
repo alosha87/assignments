9db0525b2a4b76caa534c8e520aa4dd5eff24544
Alosha Calderon <aloshacalderon@gmail.com>
2022-05-13 15:38:53 -0500

    DOM Olympics, alternate messages created.

---
+++ level-1/dom-olympics-start/index.js
@@ -0,0 +1,14 @@
+// Alternate Messages
+var first = document.getElementById("txtOne")
+    first.textContent = "Hi! How are you?";
+
+var second = document.getElementById("txtTwo")
+    second.textContent = "I'm doing great! What about you?";
+
+var third = document.getElementById("txtThree")
+    third.textContent = "Awesome!";
+
+var fourth = document.getElementById("txtFour")
+    fourth.textContent = "Keep the good work!";
+
+// Clear Button
