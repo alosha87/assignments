ff472f93c6b9482dcde9452b8e25bc1a5a9c0b91
Alosha Calderon <aloshacalderon@gmail.com>
2022-05-15 12:22:29 -0500

    Anonymous and Arrow Functions, Done.

---
+++ practice/Anonymous Functions/index.js
@@ -0,0 +1,19 @@
+// Anonymous Function = is a function with no name.
+
+function myFunc() {
+    
+}
+
+myFunc()
+
+var myOtherFunc = function() {}
+
+myOtherFunc()
+
+function handleClick() {
+
+}
+
+document.getElementById("submit-button").addEventListener("click", function() {
+
+})
---
+++ practice/Arrow Functions/index.js
@@ -0,0 +1,10 @@
+const h1 = document.getElementById("clickable")
+h1.addEventListener("click", () => {
+    h1.textContent = "clicked"    
+})
+
+const birds = ["robin", "pelican", "goose"]
+
+const bigBirds = birds.map(bird => "big " + bird)
+
+const addTwo = (first, second) => first + second
