0741cc4c7b93cad9b35df642fdf109f71b02ff9a
Alosha Calderon <aloshacalderon@gmail.com>
2022-05-17 13:24:43 -0500

    Mom's Shopping List, DONE.

--- level-1/Mom's Shopping List/index.js
+++ level-1/Mom's Shopping List/index.js
@@ -1,18 +1,29 @@
-const form = document["addItem"]
-
-document.addItem.addEventListener("submit", function(event) {
-    event.preventDefault()
-   
-    const item = form.toBuy.value
+// Select the form
+const form = document["grocery-list"]
+ 
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
     
-    newItem = createNewItem()
-    document.getElementById("list").appendChild(newItem)
+    const toBuy = form.toBuy.value;
     form.toBuy.value = ""
+
+    const ul = document.createElement('ul');
+    ul.textContent = toBuy + " "
+    document.getElementsByTagName("li")[0].appendChild(ul)
+
+    const editButton = document.createElement("button");
+    editButton.innerHTML = "edit"; 
+    document.getElementsByTagName("li")[0].appendChild(editButton)
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "delete";
+    document.getElementsByTagName("li")[0].appendChild(deleteButton)
+
+    deleteButton.addEventListener("click", removeListItem); 
+        function removeListItem(event) {
+            // console.log(event.target.parentElement) => to verify the element to be call
+            event.target.parentElement.remove();
+        }
+        
 })
-
-function createNewItem() {
-
-    
-
-}
 
