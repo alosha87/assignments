4d0f245a5a09c3a9c5001cdf9079a4659da672d8
Alosha Calderon <aloshacalderon@gmail.com>
2022-05-18 08:11:47 -0500

    Mom's Shopping List, verified js and made some notes.

--- level-1/Mom's Shopping List/index.js
+++ level-1/Mom's Shopping List/index.js
@@ -1,16 +1,19 @@
 // Select the form
 const form = document["grocery-list"]
  
+// Stop Refreshing - do this always first
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    
+
+// Adding new items on DOM
     const toBuy = form.toBuy.value;
     form.toBuy.value = ""
 
     const ul = document.createElement('ul');
     ul.textContent = toBuy + " "
     document.getElementsByTagName("li")[0].appendChild(ul)
-
+    
+// Create buttons 
     const editButton = document.createElement("button");
     editButton.innerHTML = "edit"; 
     document.getElementsByTagName("li")[0].appendChild(editButton)
@@ -19,11 +22,10 @@ form.addEventListener("submit", (event) => {
     deleteButton.innerHTML = "delete";
     document.getElementsByTagName("li")[0].appendChild(deleteButton)
 
+// Delete item when click button
     deleteButton.addEventListener("click", removeListItem); 
         function removeListItem(event) {
             // console.log(event.target.parentElement) => to verify the element to be call
-            event.target.parentElement.remove();
-        }
-        
+            event.target.parentElement.remove("li");
+        }    
 })
-
