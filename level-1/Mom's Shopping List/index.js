b180720e3ba1dc574aeb847e78ad2c3c362ea60e
Alosha Calderon <aloshacalderon@gmail.com>
2022-05-22 13:02:37 -0500

    Node Calculator assignment, DONE. Reviewed Array and Loops.

--- level-1/Mom's Shopping List/index.js
+++ level-1/Mom's Shopping List/index.js
@@ -1,31 +1,99 @@
+// // Select the form
+// const form = document["grocery-list"]
+ 
+// // Stop Refreshing - do this always first
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+
+// // Adding new items on DOM
+//     const productName = document.getElementById("title");
+//     const grocery = productName.value
+//     //const productName =title.value;
+//     form.title.value = ""
+
+//     grocery.textContent = event
+
+//     const ul = document.createElement('ul');
+//     ul.textContent = grocery + " "
+//     document.getElementsByTagName("li")[0].appendChild(ul)
+    
+// // Create buttons 
+//     const editButton = document.createElement("button");
+//     editButton.innerHTML = "edit"; 
+//     document.getElementsByTagName("li")[0].appendChild(editButton)
+
+//     const deleteButton = document.createElement("button");
+//     deleteButton.innerHTML = "delete";
+//     document.getElementsByTagName("li")[0].appendChild(deleteButton)
+
+// // Delete item when click button
+//     deleteButton.addEventListener("click", function(event){
+//         event.preventDefault();
+//         console.log(grocery.remove)
+//         removeListItem()
+//     }) 
+        
+//     function removeListItem(event) {
+//         // console.log(event.target.parentElement) => to verify the element to be call
+//         // event.target.parentElement.remove("li");
+//         grocery.remove()
+//     }     
+// })
+
+// _________________________________________________________________________
+
+//JS
 // Select the form
 const form = document["grocery-list"]
- 
+const list = document.getElementById("list")
 // Stop Refreshing - do this always first
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-
 // Adding new items on DOM
-    const toBuy = form.toBuy.value;
-    form.toBuy.value = ""
-
-    const ul = document.createElement('ul');
-    ul.textContent = toBuy + " "
-    document.getElementsByTagName("li")[0].appendChild(ul)
-    
-// Create buttons 
+    const productName = document.getElementById("title");
+    const grocery = productName.value
+    //const productName =title.value;
+    form.title.value = ""
+    //grocery.textContent = event
+    /** do this instead
+     *
+     *
+     * <li>
+     *   <p>ITEM</p>
+     *  <button>Edit</button>
+     *  <button>Delete</button>
+     * </li>
+     */
+    // create a new list item container
+    const newItem = document.createElement("li")
+    // create new p tag to hold item
+    const p = document.createElement("p")
+    p.textContent = grocery
+    /****/
+    //const ul = document.createElement('ul');
+    //ul.textContent = grocery + " "
+    //document.getElementsByTagName("li")[0].appendChild(ul)
+// Create buttons
     const editButton = document.createElement("button");
-    editButton.innerHTML = "edit"; 
-    document.getElementsByTagName("li")[0].appendChild(editButton)
-
+    editButton.innerHTML = "edit";
+    //document.getElementsByTagName("li")[0].appendChild(editButton)
     const deleteButton = document.createElement("button");
     deleteButton.innerHTML = "delete";
-    document.getElementsByTagName("li")[0].appendChild(deleteButton)
-
+    //document.getElementsByTagName("li")[0].appendChild(deleteButton)
+    // append everything to the listITem
+    newItem.append(p, editButton, deleteButton)
+    // append new item to the list so it appears in the dom inside of the UL element
+    list.append(newItem)
 // Delete item when click button
-    deleteButton.addEventListener("click", removeListItem); 
-        function removeListItem(event) {
-            // console.log(event.target.parentElement) => to verify the element to be call
-            event.target.parentElement.remove("li");
-        }    
+    deleteButton.addEventListener("click", function(event){
+        console.log(grocery.remove)
+        removeListItem()
+    })
+    function removeListItem(event) {
+        // console.log(event.target.parentElement) => to verify the element to be call
+        //document.getElementsByTagName("li")[0].remove()
+        console.log(document.getElementsByTagName("li")[0])
+        //event.target.parentElement.remove("li");
+        newItem.remove()
+    }
 })
---
+++ level-1/Node Calculator/index.js
@@ -0,0 +1,35 @@
+// console program for a node calculator
+const readlineSync = require('readline-sync');
+let result;
+
+//prompt user to type first number
+const num1 = readlineSync.question("Please enter your first number: ");
+
+// prompt user to type the operator
+const operator = readlineSync.question("Please enter the operation to perform (+, -, *, /): ");
+
+//prompt user to type second number
+const num2 = readlineSync.question("Please enter your second number: ");
+
+// Switch statement to evaluate an expression
+switch(operator) {
+    case '+':
+        result = parseInt(num1) + parseInt(num2)
+        console.log(`${num1} + ${num2} = ${result}`);
+        break;
+    case '-':
+        result = parseInt(num1) - parseInt(num2)
+        console.log(`${num1} - ${num2} = ${result}`);
+        break;
+    case '*':
+        result = parseInt(num1) * parseInt(num2)
+        console.log(`${num1} * ${num2} = ${result}`);
+        break;
+    case '/':
+        result = parseInt(num1) / parseInt(num2);
+        console.log(`${num1} / ${num2} = ${result}`);
+        break;
+    default:
+        console.log('Invalid operator');
+        break;
+}
