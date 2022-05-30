96e79949864a7196db8f9064ec0bd60e32a3e3c8
Alosha Calderon <aloshacalderon@gmail.com>
2022-05-30 16:11:28 -0500

    Mutability and value types lesson

---
+++ level-1/Practice/mutability and value types/index.js
@@ -0,0 +1,30 @@
+// Mutability and Types
+
+// Immutable - cannot be change
+    //String
+    //Numbers
+    //Booleans
+//    var age = 10
+//    var otherAge = age
+//    otherAge = 12
+//    console.log(age)
+
+// Mutable - Containers => Passed by Reference
+    //Array
+    // var arr = [1, 2, 3, 4, 5]
+    // arr[0] = 6
+    // arr = [2,3,4]
+    // console.log(arr)
+
+   //Object   
+    var person = {
+        name: "harry",
+        age: 12
+}
+   
+    // var newPerson = person
+    // newPerson.hasWand = true 
+    // console.log(person) 
+
+// typeof - returns the type of the data immediately to its right
+    console.log( typeof [1, 2, 3, 4] )
---
+++ level-1/Practice/mutability and value types/notes.md
@@ -0,0 +1,15 @@
+// Mutability - the ability to change (mutate) a piece of data
+
+    // Immutable - cannot be changed
+        String
+        Number
+        Boolean
+        
+    // Mutable - can be changed
+        Array
+        Object
+        
+// The 'typeof' operator will return the type of the data immediately to its right
+    // ex:
+        var name = "steve"
+        console.log( typeof name ) // "string"
