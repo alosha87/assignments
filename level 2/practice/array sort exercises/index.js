d02a6e78510e665901fabdbdb6a795effa718806
Alosha Calderon <aloshacalderon@gmail.com>
2022-07-14 13:35:23 -0500

    Array Sort Exercises

---
+++ level 2/practice/array sort exercises/index.js
@@ -0,0 +1,73 @@
+// Use the built-in `.sort()` method on arrays to solve these problems.
+// Feel free to copy and paste the function and tests in this assignment.
+
+
+// 1) Sort an array from smallest number to largest**
+
+function leastToGreatest(arr) {
+  arr.sort((a, b) => a - b)
+    return arr
+}
+
+console.log(leastToGreatest([1, 3, 5, 2, 90, 20])); // [1, 2, 3, 5, 20, 90]
+
+
+//----------------------------------------------------------
+
+
+// 2) Sort an array from largest number to smallest**
+
+function greatestToLeast(arr) {
+  arr.sort((a, b) => b - a)
+  return arr
+}
+
+console.log(greatestToLeast([1, 3, 5, 2, 90, 20])); // [90, 20, 5, 3, 2, 1]
+
+
+//----------------------------------------------------------
+
+
+// 3) Sort an array from shortest string to longest**
+
+function lengthSort(arr) {
+  arr.sort((a, b) => a.length - b.length)
+  return arr
+}
+
+console.log(lengthSort(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "wolf", "eaten", "family"]
+
+
+//----------------------------------------------------------
+
+
+// 4) Sort an array alphabetically**
+
+function alphabetical(arr) {
+    arr.sort((a, b) => a > b ? [0] : -1) 
+    return arr
+}
+
+console.log(alphabetical(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "eaten", "family", "wolf"]
+
+
+//----------------------------------------------------------
+
+
+// 5) Sort the objects in the array by age**
+
+function byAge(arr){
+    arr.sort((a, b) => a.age > b.age ? 0 : -1)
+    return arr
+  }
+  
+  console.log(byAge([
+      { name: "Quiet Samurai", age: 22 },
+      { name: "Arrogant Ambassador", age: 100 },
+      { name: "Misunderstood Observer", age: 2 },
+      { name: "Unlucky Swami", age: 77 }
+  ]));
+  // => [ { name: 'Misunderstood Observer', age: 2 },
+  //  { name: 'Quiet Samurai', age: 22 },
+  //  { name: 'Unlucky Swami', age: 77 },
+  //  { name: 'Arrogant Ambassador', age: 100 } ]
---
+++ level 2/practice/array sort exercises/notes.md
@@ -0,0 +1,57 @@
+Use the built-in `.sort()` method on arrays to solve these problems.
+
+Feel free to copy and paste the function and tests in this assignment.
+
+### **1) Sort an array from smallest number to largest**
+
+```
+function leastToGreatest(arr) {
+  // your code here
+}
+
+console.log(leastToGreatest([1, 3, 5, 2, 90, 20])); // [1, 2, 3, 5, 20, 90]
+
+```
+
+---
+
+### **2) Sort an array from largest number to smallest**
+
+```
+function greatestToLeast(arr) {
+  // your code here
+}
+
+console.log(greatestToLeast([1, 3, 5, 2, 90, 20])); // [90, 20, 5, 3, 2, 1]
+
+```
+
+---
+
+### **3) Sort an array from shortest string to longest**
+
+```
+function lengthSort(arr) {
+  // your code here
+}
+
+console.log(lengthSort(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "wolf", "eaten", "family"]
+
+```
+
+---
+
+### **4) Sort an array alphabetically**
+
+```
+function alphabetical(arr) {
+
+}
+
+console.log(alphabetical(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "eaten", "family", "wolf"]
+
+```
+
+---
+
+### **5) Sort the objects in the array by age**
