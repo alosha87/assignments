82c2f3840e991c1a5ef2da5e2cf5e36441850197
Alosha Calderon <aloshacalderon@gmail.com>
2022-07-21 16:31:40 -0500

    Error Handling lesson

---
+++ level 2/practice/error handling/index.js
@@ -0,0 +1,15 @@
+
+
+try {
+    
+    if(2 === 2){
+       throw new Error("Hey I am an error!")
+    }
+    console.log("Did I console log?")
+}
+catch(err){
+    console.log(err)
+}
+finally {
+   console.log("I am running no matter what!") 
+}
---
+++ level 2/practice/mid level assessment/index.js
@@ -0,0 +1,20 @@
+const people = [ "John", "Adam", "Amber" ]
+
+
+function peopleElements(arr) {
+    const result = arr.map(function(tag){
+        //return "<h1>" +  people  + "</h1>"
+        return `<h1> ${tag} </h1>` //backticks
+    })
+    return result
+}
+
+console.log(peopleElements(people))
+
+//Expected Output: [ "<h1>John</h1>", "<h1>Adam</h1>", "<h1>Amber</h1>" ]
+
+
+
+
+
+
