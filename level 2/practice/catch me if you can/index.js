83ae84f2b2a694f99ad24f71954d5bafb5258f4f
Alosha Calderon <aloshacalderon@gmail.com>
2022-07-25 14:12:01 -0500

    catch me if you can

--- level 2/practice/catch me if you can/index.js
+++ level 2/practice/catch me if you can/index.js
@@ -3,34 +3,46 @@
 
 
 function sum(x, y){
-    if(typeof num1 !== 1 || typeof num2 !== 2); {
-        throw new Error (keep trying);//check data types first and throw error
-    return x + y;  
-} 
-    try {
-        if(x === 1 || y === 2){
-            throw "Error"
-        }
-        console.log(sum)
-    }
+    if(typeof x !== 'number' || typeof y !== 'number'){
+        throw new Error ("Error! Numbers only!");         //check data types first and throw error
+    } 
+    return x + y;
+    }    
 
-    catch(err) {
-        console.log("err")
+    try {
+        sum("1", "2")
+    }    
+
+    catch(err){
+        console.log(err)        
     }
 
     finally {
         console.log("Run no matter what!")
     }
 
+
   
-
-
 // 2a) Given a user object, write a function called `login` that takes a `username` and `password` as parameters. Throw an error if either of them don't match. Otherwise, log to the console a message saying `"login successful!"`  
 
-var user = {username: "sam", password: "123abc"};
+var user = {
+    username: "sam", 
+    password: "123abc"};
+
 function login(username, password){
-  //check credentials
+    if(username !== user.username || password !== user.password){   //check credentials
+        throw Error ("Information doesn't match with our system!")   
+    } else {
+        console.log('login successfull!')
+    }    
 }
+    try{
+        login('sam', '123abc')
+    } 
+    
+    catch(err){
+        console.log(err)
+    }
 
 
-// 2b) Call the login function within a try block. In one instance use the correct credentials, and in another use incorrect ones. Make sure you see the appropriate message!
+// 2b) Call the login function within a try block. In one instance use the correct credentials, and in another use incorrect ones. Make sure you see the appropriate message! 
