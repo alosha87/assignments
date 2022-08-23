d508826ec9d46d7b12c924d8479d280421b8b335
Alosha Calderon <aloshacalderon@gmail.com>
2022-08-23 13:17:31 -0500

    Slope Style

--- level 2/practice/slope style/app.js
+++ level 2/practice/slope style/app.js
@@ -235,3 +235,118 @@ function combineFruit(fruits, sweets, vegetables) {
 
 
 // //***************************************************************************** */
+// Use destructuring to use the property names as variables. Destructure the object in the parameter:
+//HINT: This function is expecting two variables to be defined in the parameter list.
+const itinerary = {
+    location: "Burly Idaho",
+    duration: "2 weeks"
+  }
+
+
+function parseSentence({location, duration}){
+    return `We're going to have a good time in ${location} for ${duration}`
+  }
+  
+ console.log(parseSentence(itinerary))
+
+  
+
+//   // //***************************************************************************** */
+//Use array destructuring to make this code ES6:
+const returnFirst = (items) => {
+    const [firstItem, secondItem, thirdItem] = items /*change this line to be es6*/
+    return `I'll bring a ${firstItem}, a ${secondItem}, and a ${thirdItem} to the meeting.`
+}
+console.log(returnFirst(["book", "pen", "paper"]))
+
+
+
+//   // //***************************************************************************** */
+// //Write destructuring code to assign variables that will help us return the expected string. Also, change the string to be using Template literals:
+// const favoriteActivities = ["magnets", "snowboarding", "philanthropy", "janitor work", "eating"];
+
+// function returnFavorites(arr){
+//     /*your code here*/
+//     return "My top three favorite activities are, " + firstFav + ", " + secondFav + ", and " + thirdFav";
+// }
+
+// returnFavorites(favoriteActivities)
+
+const favoriteActivities = ["magnets", "snowboarding", "philanthropy", "janitor work", "eating"];
+
+const returnFavorites = ([firstFav, secondFav, thirdFav]) => {
+    /*your code here*/
+    return `My top three favorite activities are: ${firstFav}, ${secondFav}, and ${thirdFav}`
+}
+
+console.log(returnFavorites(favoriteActivities))
+
+
+
+//   // //***************************************************************************** */
+// //Use the Rest and Spread Operator to help take any number of arrays, and return one array. You will need to change how the arrays are passed in. You may write it assuming you will always recieve three arrays if you would like to.
+// function combineAnimals() {
+
+// }
+
+// const realAnimals = ["dog", "cat", "mouse"];
+// const magicalAnimals = ["jackolope"];
+// const mysteriousAnimals = ["platypus"];
+
+// combineAnimals(realAnimals, magicalAnimals, mysteriousAnimals);
+
+//OUTPUT:
+// // ["dog", "cat", "mouse", "jackolope", "platypus"]
+
+const combineAnimals = (...animals) => {
+    return animals
+    }
+    
+    const realAnimals = ["dog", "cat", "mouse"];
+    const magicalAnimals = ["jackolope"];
+    const mysteriousAnimals = ["platypus"];
+    
+    combineAnimals(realAnimals, magicalAnimals, mysteriousAnimals);
+
+console.log(combineAnimals(...realAnimals, ...magicalAnimals, ...mysteriousAnimals))
+
+//   // //***************************************************************************** */
+// //Try to make the following function more ES6y:
+// function product(a, b, c, d, e) {
+//         var numbers = [a,b,c,d,e];
+      
+//         return numbers.reduce(function(acc, number) {
+//           return acc * number;
+//         }, 1)
+//       }
+
+  
+
+//   // //***************************************************************************** */
+// //   Make the following function more ES6y. Use at least both the rest and spread operators:
+// function unshift(array, a, b, c, d, e) {
+//     return [a, b, c, d, e].concat(array);
+//   }
+  
+
+
+
+// //   
+// // Write some destructuring code to help this function out. Use the ES6 shorthand that helps make the syntax look less redundant to simplify it:
+// function populatePeople(names){
+//     return names.map(function(name){
+//         name = name.split(" ");
+//         // your code
+//         return {
+//             firstName: firstName,
+//             lastName: lastName
+//         }
+//     })
+// }
+
+// populatePeople(["Frank Peterson", "Suzy Degual", "Liza Jones"])
+// //[
+// //  {firstName: "Frank", lastName: "Peterson"},
+// //  {firstName: "Suzy", lastName: "Degual"},
+// //  {firstName: "Liza", lastName: "Jones"},
+// //]
